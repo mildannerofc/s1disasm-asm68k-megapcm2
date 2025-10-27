5995cb7599e5c8ef7aa7975139ffc43dc37b415b
Mildanner <cianocriativo408@gmail.com>
2025-10-26 23:02:42 -0300

    Initial

---
+++ SampleTable.asm
@@ -0,0 +1,26 @@
+
+; ---------------------------------------------------------------
+SampleTable:
+	;			type			pointer		Hz
+	dcSample	TYPE_DPCM, 		Kick, 		8000				; $81
+	dcSample	TYPE_PCM,		Snare,		24000				; $82
+	dcSample	TYPE_DPCM, 		Timpani, 	7250				; $83
+	dcSample	TYPE_PCM,		Clap,		17000					; $84
+	dcSample	TYPE_PCM,		Cymbal,		16534					; $85
+	dcSample	TYPE_NONE										; $86
+	dcSample	TYPE_NONE										; $87
+	dcSample	TYPE_DPCM, 		Timpani, 	9750				; $88
+	dcSample	TYPE_DPCM, 		Timpani, 	8750				; $89
+	dcSample	TYPE_DPCM, 		Timpani, 	7150				; $8A
+	dcSample	TYPE_DPCM, 		Timpani, 	7000				; $8B
+	dcSample	TYPE_PCM,		SegaPCM,	0, FLAGS_SFX		; $8C	NOTE: sample rate is auto-detected from WAV file
+	dc.w	-1	; end marker
+
+; ---------------------------------------------------------------
+	incdac	Kick, "sound/dac/kick.dpcm"
+	incdac	Snare, "sound/dac/snare.pcm"
+	incdac	Timpani, "sound/dac/timpani.dpcm"
+	incdac	Clap, "sound/dac/Clap.wav"
+	incdac	Cymbal, "sound/dac/Cymbal.wav"
+	incdac	SegaPCM, "sound/dac/sega.wav"
+	even
